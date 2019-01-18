0e4cad385c450f21d6baa2b536c766f9c6d7a579
Gbardis <crisolivasavatage@hotmail.com>
2019-01-18 14:47:54 +0200

    Add create action for authors controller

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,3 +1,16 @@
 class AuthorsController < ApplicationController
-  def new; end
+  def create
+    author = Author.new(author_params)
+    if author.save
+      render json: author, status: :ok
+    else
+      render json: { errors: author.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def author_params
+    params.permit(:id, :email, :first_name, :last_name, :date_of_birth)
+  end
 end
